651749d5679a258cc52c7546c9a9123f8cb612b9
k <katerinab+hs@gmail.com>
2013-12-01 22:36:07 -0500

    Removed stale code, have execution working with recursive codegen

--- minilisp.py
+++ minilisp.py
@@ -8,6 +8,7 @@ prompt = ">"
 
 Symbol = str
 
+# The parser/tokenizer/read_from are stolen from Norvig's lis.py
 def parse(x):
     tokens = (tokenize(x))
     if tokens:
@@ -15,11 +16,12 @@ def parse(x):
     else:
         return []
 
+
 def tokenize(x):
     return x.replace('(', ' ( ').replace(')', ' ) ').split()
 
+
 def read_from(tokens):
-    print tokens
     if len(tokens) == 0:
         raise SyntaxError("Unexpected EOF (read_from)")
     cur_token = tokens.pop(0)
@@ -32,11 +34,10 @@ def read_from(tokens):
     elif cur_token == ')':
         raise SyntaxError("Unexpected ')' (read_from)")
     else:
-        print "cur_token is %s" % cur_token
         return atom(cur_token)
    
+
 def atom(token):
-    print "atomizing %s" % token
     try:
         return int(token)
     except ValueError:
@@ -44,42 +45,7 @@ def atom(token):
             return float(token)
         except ValueError:
             return Symbol(token) 
-    
-def compile_and_execute(afunc, args):
-    afn = afunc.__name__
-    a1 = args[0]
-    a2 = args[1]
 
-    if not hasattr(compile_and_execute, 'compiled_functions'):
-        compile_and_execute.compiled_functions = {}
-    cf = compile_and_execute.compiled_functions
-
-    if afn not in cf:
-        (module, compiled_func) = compile(afunc, a1, a2)
-        cf[afn] = (module, compiled_func)
-        #print "compiled is %s" % comp_mod[0]
-    else:
-        print "Function cache hit!"
-    executed = execute(cf[afn][0], cf[afn][1], a1, a2)
-    #print "executed is %s" % executed.as_int()
-    return executed
-
-
-def applyit(afunc, args):
-    return apply(afunc, args[:2]) # FIXME
-
-def old_evaluate(aparse, compiled_funcs=None):
-    if not isinstance(aparse, [].__class__):
-        return aparse
-    else:
-        afunc = lookup(aparse[0])
-        ret = compile_and_execute(afunc, aparse[1:3])
-
-        #comp_mod = compile(afunc, aparse[1], aparse[2])
-        #print "compiled is %s" % comp_mod[0]
-        #executed = execute(comp_mod[0], comp_mod[1], aparse[1], aparse[2])
-        print "executed is %s" % ret.as_int()
-        return applyit(afunc, aparse[1:])
 
 def is_integer(atom):
     try:
@@ -88,10 +54,13 @@ def is_integer(atom):
     except ValueError:
         return False
 
+
 def is_atom(aparse):
     return not isinstance(aparse, [].__class__) 
 
+
 current_bb_builder = None
+
 
 def codegen(aparse):
     if is_atom(aparse):
@@ -99,71 +68,54 @@ def codegen(aparse):
             return llvm.core.Constant.int(llvm.core.Type.int(), aparse)
         else:
             raise ValueError("unhandled atom")
-    else: # everything else is currently a two-argument function
+    else: # everything else is currently a no-argument function
         lint = llvm.core.Type.int()
         #two_arg_func = llvm.core.Type.function(lint, [lint, lint])
+        op = lookup(aparse[0])
         a1 = codegen(aparse[1])
         a2 = codegen(aparse[2])
-        tmp = getattr(current_bb_builder, operator.add.__name__)(a1, a2, "tmpwhy")
+        tmp = getattr(current_bb_builder, op.__name__)(a1, a2, "tmpwhy")
         return tmp
         
+
 def compile_line(aparse):
     lisp_module = llvm.core.Module.new("minilisp")
     lint = llvm.core.Type.int()
-    myfunc = llvm.core.Type.function(lint, [])
-    f = lisp_module.add_function(myfunc, "afunctionname")
+    func_type = llvm.core.Type.function(lint, [])
+    f = lisp_module.add_function(func_type, "afunction")
     bb = f.append_basic_block("entry")
     global current_bb_builder
     current_bb_builder = llvm.core.Builder.new(bb)
     current_bb_builder.ret(codegen(aparse))
     print "module: %s" % lisp_module
-    print "function: %s" % myfunc
-    return myfunc
+    print "function: %s" % f
+    return lisp_module, f
 
-def execute(module, llvmfunc, arg1, arg2):
+
+def execute(module, llvmfunc):
     ee = llvm.ee.ExecutionEngine.new(module)
     lint = llvm.core.Type.int()
-    a1 = llvm.ee.GenericValue.int(lint, arg1)
-    a2 = llvm.ee.GenericValue.int(lint, arg2)
-   
-    print "llvmfunc: %s" % llvmfunc
-    print "a1: %s" % a1 
-    retval = ee.run_function(llvmfunc, [a1, a2])
-    print "retval is %s" % retval
+    retval = ee.run_function(llvmfunc, [])
+    print "retval is %s" % retval.as_int()
     return retval
 
-def old_compile(afunc, arg1, arg2): # FIXME; should this take the module as an arg?
-    lisp_module = llvm.core.Module.new("minilispmod")
-    lint = llvm.core.Type.int()
-    two_arg_func = llvm.core.Type.function(lint, [lint, lint])
-    
-    f = lisp_module.add_function(two_arg_func, afunc.__name__)
-    #print dir(f.args[0])
-    f.args[0].name = 'a'
-    f.args[1].name = 'b'
-
-    bb = f.append_basic_block("entry")
-    builder = llvm.core.Builder.new(bb)
-    #print dir(builder)
-    tmp = getattr(builder, afunc.__name__)(f.args[0], f.args[1], "tmpwhy")
-    builder.ret(tmp)
-
-    return (lisp_module, f)
 
 def lookup(afunc): # FIXME
-    funcs = {'+':operator.add, '*':operator.mul}
+    funcs = {'+':operator.add, '*':operator.mul, '-':operator.sub}
     if afunc in funcs:
         return funcs[afunc]
     else:
         raise ValueError("Undefined function %s" % afunc)
 
+
 def repl():
     while True:
-        try:
+        #try:
             line = raw_input("%s " % prompt)
-            print evaluate(parse(line), compiled_funcs = {})
-        except ValueError as ve:
-            print ve
+            m, f = compile_line(parse(line))
+            execute(m, f)
+        #except ValueError as ve:
+        #    print ve
             
 
 if __name__ == '__main__':
